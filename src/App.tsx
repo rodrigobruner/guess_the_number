96b5d376e38679a0ad703a4ddc80c60c68008a04
Rodrigo Bruner <me@bruner.app>
2025-10-17 12:20:16 -0400

    feat: guess the number single player

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,46 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [maxNumber, setMaxNumber] = useState(10);
+  const [magicNumber, setMagicNumber] = useState(Math.floor(Math.random() * 11));
+  const [guess, setGuess] = useState<Number|''>('');
+  const [displayMagicNumber, setDisplayMagicNumber] = useState<boolean>(false);
+  const [msg, setMsg] = useState<string>('');
+
+  function handleSubmit(event: React.FormEvent){
+    event.preventDefault();
+  }
+
+  function handleDisplayMagicNumber(){
+      setDisplayMagicNumber(displayMagicNumber ? false:true);
+      setMsg('');
+      winner();
+  }
+
+  function winner(){
+    if (guess == magicNumber){
+      setMsg("You win!");
+    } else {
+      setMsg("Try again");
+    }
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>{displayMagicNumber ? msg : ''}</h2>
+        <h1>
+          {displayMagicNumber ? magicNumber : '#?'}          
+        </h1>
+        <button onClick={handleDisplayMagicNumber}>{displayMagicNumber ? 'Hide':'Show'} magic number</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <p>Enter a number between 0 and {maxNumber}</p>
+        <input  type='number' onChange={(e) => { setGuess(Number(e.target.value))} } />
+        <button type='submit'>Submit my guess</button>
+      </form>
     </>
   )
 }
