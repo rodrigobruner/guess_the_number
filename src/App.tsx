4121de41fb73555a1a1cde929cd2cac2e0e6eca6
Rodrigo Bruner <me@bruner.app>
2025-10-19 17:26:17 -0400

    feat: enhance guessing game functionality with user guess tracking and removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,60 @@
-import { useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import './App.css'
+
+type UserGuess = {
+  username: string;
+  guess: number;
+}
 
 function App() {
 
   const [maxNumber, setMaxNumber] = useState(10);
-  const [magicNumber] = useState(Math.floor(Math.random() * 11));
-  const [guess, setGuess] = useState<Number|''>('');
+  const [magicNumber, setMagicNumber] = useState<number>(0);
   const [displayMagicNumber, setDisplayMagicNumber] = useState<boolean>(false);
   const [msg, setMsg] = useState<string>('');
 
+  const [guess, setGuess] = useState<number>(0);
+  const [username, setUsername] = useState<string>('');
+  const [guesses, addGuess] = useState<UserGuess[]>([]);
+
+  const [ranking, setRanking] = useState<UserGuess[]>([]);
+  
+
+
+  useEffect(() => {
+    setMagicNumber(Math.floor(Math.random() * maxNumber+1));
+  }, [maxNumber]);
+
+  function generateNewNumber(){
+    setMagicNumber(Math.floor(Math.random() * maxNumber+1));
+  }
+
   function handleSubmit(event: React.FormEvent){
     event.preventDefault();
+    addGuess([...guesses, { username, guess }]);
   }
+
 
   function handleDisplayMagicNumber(){
       setMaxNumber(10); 
+      if(displayMagicNumber){
+      generateNewNumber();
+      }
       setDisplayMagicNumber(displayMagicNumber ? false:true);
-      setMsg('');
-      winner();
+      checkWinner();
   }
 
-  function winner(){
-    if (guess == magicNumber){
-      setMsg("You win!");
-    } else {
-      setMsg("Try again");
-    }
+
+  function handleRemoveGuess(indexToRemove: number){
+    const newGuesses = guesses.filter((_, index) => index !== indexToRemove);
+    addGuess(newGuesses);
+  }
+
+  function checkWinner(){
+    const neartestGuess = guesses.reduce((prev, curr) => {
+      return (Math.abs(curr.guess - magicNumber) < Math.abs(prev.guess - magicNumber) ? curr : prev);
+    }, guesses[0]);
+    setMsg('Winner: '+ neartestGuess.username + ' with guess ' + neartestGuess.guess);
   }
 
   return (
@@ -40,9 +69,19 @@ function App() {
       <hr></hr>
       <form onSubmit={handleSubmit}>
         <p>Enter a number between 0 and {maxNumber}</p>
+        <input type='text' onChange={(e) => { setUsername(e.target.value)} } />
         <input  type='number' onChange={(e) => { setGuess(Number(e.target.value))} } />
-        <button type='submit'>Submit my guess</button>
+        <button type='submit'>Add guess</button>
       </form>
+
+      {
+        guesses.map((g, index) => (
+          <div key={index}>
+            <p>{g.username} guessed {g.guess} <button onClick={() => handleRemoveGuess(index)}>Remove</button></p>
+          </div>
+        ))
+      }
+
     </>
   )
 }
